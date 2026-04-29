967447ac3045c5ceac693cfd781ca068219160bc
beberlyjulissaccastro-bit <beberlyjulissaccastro@gmail.com>
2026-04-29 16:02:24 -0600

    primer cambio

---
+++ tercera_practica.js
@@ -0,0 +1,8 @@
+let cantidad = document.getElementById("cantidad")
+let suma = document.getElementById("suma")
+let resta = document.getElementById("resta")
+
+suma.addEventListener("click",ver)
+resta.addEventListener("click", ver)
+
+function ver ()
